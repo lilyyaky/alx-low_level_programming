961faba40a78de90370033dc540d26b8747c6401
lilyyaky <yaklily0@gmail.com>
2022-06-21 11:53:09 -0700

    new

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *s0 = "Lilian Yakubu";
+    char *s1 = "Tiannah David";
+
+    printf("%s, %s\n", s0, s1);
+    set_string(&s1, s0);
+    printf("%s, %s\n", s0, s1);
+    return (0);
+}
